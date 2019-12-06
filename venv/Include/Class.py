e4c3f58ac172ea617d01bdcce27cab0ee864239c
Jakub <gudzikoff@gmail.com>
2019-12-07 00:05:39 +0100

    Generators

--- venv/Include/Class.py
+++ venv/Include/Class.py
@@ -1,34 +1,16 @@
-def replacing_Control_Flow(score = None):
-    return (score == 1 and "Winner") or (score == -1 and "Loser") or "Tied"
+def generate_triangles():
+    x = 0
+    y = 0
+    while True:
+        x += y
+        y += 1
+        yield x
 
-print(replacing_Control_Flow())
-print(replacing_Control_Flow(1))
-print(replacing_Control_Flow(-1))
-print()
 
-def dot_product(u, v):
-    return sum(list(map(lambda x: x[0] * x[1],zip(u,v))))
+def triangles_under(x):
+    for triangle in generate_triangles():
+        if triangle >= x:
+            break
+        print(triangle)
 
-print(dot_product([1, 3, 5], [2, 4, 6]))
-print()
-
-def transpose(m):
-    return tuple(zip(*m))
-
-matrix = (
-    (1, 2, 3, 4),
-    (5, 6, 7, 8),
-    (9,10,11,12)
-)
-print(transpose(matrix))
-
-def matmul(m1, m2):
-    return tuple(map(lambda row: tuple(dot_product(row, col) for col in transpose(m2)), m1))
-
-matrix1 = (
-    (1, 2, 3, 4),
-    (5, 6, 7, 8),
-    (9,10,11,12)
-)
-
-print(matmul(matrix,matrix1))
+triangles_under(20)
