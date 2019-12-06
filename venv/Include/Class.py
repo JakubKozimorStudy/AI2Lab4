22e48275417d5140de03fcbde4b4bbe8857ca586
Jakub <gudzikoff@gmail.com>
2019-12-06 22:32:45 +0100

    Other Useful Tools

--- venv/Include/Class.py
+++ venv/Include/Class.py
@@ -1,8 +1,41 @@
-def filter_task():
-    print(list(filter(lambda n : int(n) >= 0,['12', '-2', '0'])))
-    print(list(filter(lambda n : n == 'world',['hello', 'world'])))
-    print(list(filter(lambda n : n == 'Stanford',['Stanford', 'Cal', 'UCLA'])))
-    print(list(filter(lambda n : n%3 == 0 or n%5 == 0,range(20))))
+import operator
+from functools import reduce
+
+def gcd(a, b):
+    """Reference implementation of finding the
+    greatest common denominator of two numbers"""
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def lcm(*args):
+    return reduce(lambda x, y: x * y / gcd(x, y), args)
 
 
-filter_task()
+def fact(n):
+    return reduce(operator.mul, range(1,n+1))
+
+print(lcm(3,5))
+
+print(operator.add(1, 2))
+print(operator.mul(3, 10))
+print(operator.pow(2, 3) )
+print(operator.itemgetter(1)([1, 2, 3]))
+
+
+print(fact(3))  # => 6
+print(fact(7))  # => 5040
+
+def alpha_score(upper_letters):
+    """Computers the alphanumeric sum of letters in a string.
+    Prerequisite: upper_letters is composed entirely of capital letters.
+    """
+    return sum(map(lambda l: 1 + ord(l) - ord('A'), upper_letters))
+
+print(alpha_score('ABC'))  # => 6 = 1 ('A') + 2 ('B') + 3 ('C')
+
+def two_best(words):
+    words.sort(key=lambda word: alpha_score(filter(str.isupper, word)), reverse=True)
+    return words[:2]
+
+print(two_best(['hEllO', 'wOrLD', 'i', 'aM', 'PyThOn']))
